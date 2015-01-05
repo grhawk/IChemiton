7c0f7eab0c93e7d30caf5ebe465513ab32656241
Riccardo Petraglia <riccardo.petraglia@epfl.ch>
2015-01-05 16:29:22 +0100

    Add the profile

---
+++ profile_labbook/startup/00StartupMessage.py
@@ -0,0 +1,3 @@
+from __future__ import print_function
+
+print('\nLoading the **Labbok** profile')
---
+++ profile_labbook/startup/50MostUsedModules.py
@@ -0,0 +1,11 @@
+from __future__ import print_function
+
+import IPython.utils as ut
+import sys, os
+
+sys.path.append(os.path.join(ut.path.locate_profile('labbook'), 'modules'))
+
+print('\nModules:\n cinfony\n cinfony -> rdk, pybel\n PyMol')
+from cinfony import rdk, pybel  #https://code.google.com/p/cinfony/
+import PyMol
+import cinfony
