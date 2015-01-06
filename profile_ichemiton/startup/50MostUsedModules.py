852821558064e65e31f6cca1210b776cb043390c
Riccardo Petraglia <riccardo.petraglia@epfl.ch>
2015-01-06 15:00:11 +0100

    Modified to work with the new name profile

--- profile_ichemiton/startup/50MostUsedModules.py
+++ profile_ichemiton/startup/50MostUsedModules.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import IPython.utils as ut
 import sys, os
 
-sys.path.append(os.path.join(ut.path.locate_profile('labbook'), 'modules'))
+sys.path.append(os.path.join(ut.path.locate_profile('ichemiton'), 'modules'))
 
 print('\nModules:\n cinfony\n cinfony -> rdk, pybel\n PyMol')
 from cinfony import rdk, pybel  #https://code.google.com/p/cinfony/
