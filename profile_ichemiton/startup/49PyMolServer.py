07c7220421595675afae4c349a9827671bd30a1c
Riccardo Petraglia <riccardo.petraglia@epfl.ch>
2015-01-08 11:50:29 +0100

    Pymol now starts only if needed

--- profile_ichemiton/startup/49PyMolServer.py
+++ profile_ichemiton/startup/49PyMolServer.py
@@ -1,9 +1,18 @@
 ## Run the pymol server
 from __future__ import print_function
+import xmlrpclib
 
-print('\nStarting PyMol as server... ')
-import subprocess as sp
-import os
-FNULL = open(os.devnull, 'w')
-sp.Popen(['pymol', '-R'], stdout=FNULL, stderr=sp.STDOUT)
-print('...Done')
+try:
+    serv = xmlrpclib.Server('http://%s:%d'%('localhost',9123))
+    serv.ping()
+except:
+    print('\nStarting PyMol as server... ')
+    import subprocess as sp
+    import os
+    import time
+    FNULL = open(os.devnull, 'w')
+    sp.Popen(['pymol', '-R'], stdout=FNULL, stderr=sp.STDOUT)
+    time.sleep(3)
+    print('...Done')
+else:
+    print('\nPymol already running')
