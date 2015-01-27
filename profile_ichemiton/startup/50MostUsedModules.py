ed8264923e281c071bcfa7744d619d958f46cf7d
Riccardo Petraglia <riccardo.petraglia@epfl.ch>
2015-01-27 14:56:17 +0100

    Added matplotlib among the imported modules

--- profile_ichemiton/startup/50MostUsedModules.py
+++ profile_ichemiton/startup/50MostUsedModules.py
@@ -15,6 +15,7 @@ import cinfony
 print('\nMath Modules:\n numpy as np\n mathplotlib.pyplot as plt\n pandas as pd')
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import pandas as pd
 # Settings for pandas
 pd.set_option('display.notebook_repr_html', True) # Print the data directly in a nice table
